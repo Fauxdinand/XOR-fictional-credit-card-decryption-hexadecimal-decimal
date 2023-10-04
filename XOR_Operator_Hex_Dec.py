feeaacc7262461cb493984959db2098ce760356e
Fauxdinand <116094693+Fauxdinand@users.noreply.github.com>
2023-10-04 15:02:47 +0200

    Update XOR_Operator_Hex_Dec.py

--- XOR_Operator_Hex_Dec.py
+++ XOR_Operator_Hex_Dec.py
@@ -5,25 +5,27 @@ def xorcount():
     before adding it to a list
 
     """
-    count=0 
-    deci=0
-    hexa=0
-    hexalist = []
+    c=0 
+    d=0
+    h=0
+    hl = []
     try:
-        while count !=(16):
+        while c!=(16):
     
-            deci = int(input('Enter dec: '))
-            hexa = int(input('Enter hex: '), 16)
-            xor = ((deci)^(hexa))
-            noprefix = int(xor)
-            hexalist.append(noprefix)
-            count +=1
+            d = int(input('Enter dec: '))
+            h = int(input('Enter hex: '), 16)
+            xr = ((d)^(h))
+            np = format(xr, 'x')
+            hs = np.upper()
+            hl.append(hs)
+            c +=1
     except ValueError:
             print("\N{Duck} says: Read documentation")
 
     else:
-        print(f'\033[95m \033[92m\N{Duck} says: "XOR-operator finished: "\033[0m')
-        print(f'\033[1m {hexalist} \033[0m')
+        l = ' '.join(hl)
+        print(f'\033[95m \033[92m\N{Duck} says: XOR-operator finished: \033[0m')
+        print(f'\033[1m {l} \033[0m')
 
 
-xorcount()
+# xorcount()
