cb0e092b67c592ab4b50dbb1bead7edbcfdd22cb
Fauxdinand <116094693+Fauxdinand@users.noreply.github.com>
2023-10-04 17:43:32 +0200

    Update XOR_Operator_Hex_Dec.py

--- XOR_Operator_Hex_Dec.py
+++ XOR_Operator_Hex_Dec.py
@@ -1,3 +1,4 @@
+
 def xorcount():
     """ 
     Only 1 digits at the time. First decimal and then hexadecimal
@@ -5,25 +6,29 @@ def xorcount():
     before adding it to a list
 
     """
-    c=0 
     d=0
     h=0
+    dl = []
     hl = []
+    xl = []
     try:
-        while c!=(16):
+        while len(dl) !=(3):  
             d = int(input('Enter \033[1m\033[94mdec \033[0m: '))
+            dl.append(d)
+        while len(hl) !=(3):               
             h = int(input('Enter \033[1m\033[92mhex \033[0m: '), 16)
-            xr = ((d)^(h))
-            np = format(xr, 'x')
-            hs = np.upper()
-            hl.append(hs)
-            c +=1
+            hl.append(h)
+        for i in range(0, len(dl)):
+            xi = (dl[i] ^ hl[i])
+            xf = format(xi, 'x')
+            xb = xf.upper()
+            xl.append(xb)
     except ValueError:
             print("\N{Duck} says: Read documentation")
     else:
-        l = ' '.join(hl)
+        xp = ' '.join(xl)
         print(f'\033[95m \033[92m\N{Duck} says: XOR-operator finished: \033[0m')
-        print(f'\033[1m {l} \033[0m')
+        print(f'\033[1m {xp} \033[0m')
 
 
-# xorcount()
+xorcount()
