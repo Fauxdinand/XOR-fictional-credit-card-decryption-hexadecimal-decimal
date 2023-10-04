090437084812006ffb2bd28b27d5a5139493ced1
Fauxdinand <116094693+Fauxdinand@users.noreply.github.com>
2023-10-04 15:04:47 +0200

    Update XOR_Operator_Hex_Dec.py

--- XOR_Operator_Hex_Dec.py
+++ XOR_Operator_Hex_Dec.py
@@ -11,7 +11,6 @@ def xorcount():
     hl = []
     try:
         while c!=(16):
-    
             d = int(input('Enter dec: '))
             h = int(input('Enter hex: '), 16)
             xr = ((d)^(h))
@@ -21,7 +20,6 @@ def xorcount():
             c +=1
     except ValueError:
             print("\N{Duck} says: Read documentation")
-
     else:
         l = ' '.join(hl)
         print(f'\033[95m \033[92m\N{Duck} says: XOR-operator finished: \033[0m')
