327dca3cd2e239741c8c0cf83d55bdb0fc04c882
Fauxdinand <116094693+Fauxdinand@users.noreply.github.com>
2023-10-06 15:12:16 +0200

    Update XOR_Operator_Hex_Dec.py

--- XOR_Operator_Hex_Dec.py
+++ XOR_Operator_Hex_Dec.py
@@ -1,34 +1,32 @@
-
 def xorcount():
     """ 
-    Only 1 digit at the time. First 16 x decimal, then 16 x hexadecimal.
-    The program will then extract one item per list and perform a XOR-operation on each pair 
-    before adding it to a new list
+    Only 1 digits at the time. First decimal and then hexadecimal
+    so the program can perform a XOR-operation on each pair 
+    before adding it to a list
 
     """
-    d=0
-    h=0
-    dl = []
-    hl = []
-    xl = []
+
+    d = []
+    h = []
+    x = []
     try:
-        while len(dl) !=(16):  
-            d = int(input('Enter \033[1m\033[94mdec \033[0m: '))
-            dl.append(d)
-        while len(hl) !=(16):               
-            h = int(input('Enter \033[1m\033[92mhex \033[0m: '), 16)
-            hl.append(h)
-        for i in range(0, len(dl)):
-            xi = (dl[i] ^ hl[i])
-            xf = format(xi, 'x')
-            xb = xf.upper()
-            xl.append(xb)
+        while len(d) !=(16):  
+            _ = int(input('Enter \033[1m\033[94mdec \033[0m: '))
+            d.append(_)
+        while len(h) !=(16):               
+            _ = int(input('Enter \033[1m\033[92mhex \033[0m: '), 16)
+            h.append(_)
+        for i in range(0, len(d)):
+            _ = (d[i] ^ h[i])
+            _ = format(_, 'x')
+            _ = _.upper()
+            x.append(_)
     except ValueError:
             print("\N{Duck} says: Read documentation")
     else:
-        xp = ' '.join(xl)
+        _ = ' '.join(x)
         print(f'\033[95m \033[92m\N{Duck} says: XOR-operator finished: \033[0m')
-        print(f'\033[1m {xp} \033[0m')
+        print(f'\033[1m {_} \033[0m')
 
 
-xorcount()
+# xorcount()
